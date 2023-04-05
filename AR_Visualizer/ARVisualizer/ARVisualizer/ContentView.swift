32a24a6f2fedb40291d6a9ca0fb0d437edf5b7fa
DanMark <daniel.mark.kiss@gmail.com>
2023-04-05 10:12:50 +0200

    text mesh not working

--- AR_Visualizer/ARVisualizer/ARVisualizer/ContentView.swift
+++ AR_Visualizer/ARVisualizer/ARVisualizer/ContentView.swift
@@ -13,9 +13,9 @@ struct ContentView : View {
     var body: some View {
         ZStack{
             ARViewContainer().edgesIgnoringSafeArea(.all)
-            Button("GOMB") {
+            /*Button("GOMB") {
                 print("Nothing")
-            }
+            }*/
         }
         
     }
@@ -31,20 +31,34 @@ struct ARViewContainer: UIViewRepresentable {
         let boxAnchor = try! Experience.loadBox()
         
         // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        //arView.scene.anchors.append(boxAnchor)
         
         let sphereResource = MeshResource.generateSphere(radius: 0.05)
-        let boxResource = MeshResource.generateBox(size: 0.08)
+        //_ = MeshResource.generateBox(size: 0.08)
+        
+        let ownTextObjectVertices = MeshResource.generateText(
+            "HALO",
+            extrusionDepth: 0.25,
+            font: .systemFont(ofSize: MeshResource.Font.systemFontSize),
+            containerFrame: CGRect.init(x: 0.0, y: 0.0, width: 1.0, height: 1.0),
+            alignment:  .left,
+            lineBreakMode: .byTruncatingTail
+        )
         
         let myMaterial = SimpleMaterial(color: .blue, roughness: 0, isMetallic: true)
         
+        let myMaterial2 = SimpleMaterial(color: .red, roughness: 0, isMetallic: true)
+        
         let myEntity = ModelEntity(mesh: sphereResource, materials: [myMaterial])
+        
+        let ownTextModel = ModelEntity(mesh: ownTextObjectVertices, materials: [myMaterial2])
         
         // Create a new Anchor Entity using Identity Transform.
         let anchorEntity = AnchorEntity()
 
         // Add the entity as a child of the new anchor.
-        anchorEntity.addChild(myEntity)
+        //anchorEntity.addChild(myEntity)
+        anchorEntity.addChild(ownTextModel)
 
         // Add the anchor to the scene.
         arView.scene.addAnchor(anchorEntity)
